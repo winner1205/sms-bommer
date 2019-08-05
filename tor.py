dd1c89701c58bcd757104a8a36420259261a99f2
winner1205 <49068008+winner1205@users.noreply.github.com>
2019-08-05 22:05:46 +0530

    Update tor.py

--- tor.py
+++ tor.py
@@ -6,7 +6,8 @@ def get_current_ip():
   session = requests.session() 
   # TO Request URL with SOCKS over TOR 
   session.proxies = {} 
-  session.proxies['http']='socks5h://localhost:9050' session.proxies['https']='socks5h://localhost:9050' 
+  session.proxies['http']='socks5h://localhost:9050' 
+  session.proxies['https']='socks5h://localhost:9050' 
   try: 
     r = session.get('http://httpbin.org/ip') 
   except Exception as e: 
